01ef39a36894877b44b294c505d8cec7879481bd
4riouha <4riuha@gmail.com>
2023-07-31 19:34:28 +0330

    .

--- src/modules/currecies/cron.service.ts
+++ src/modules/currecies/cron.service.ts
@@ -6,10 +6,10 @@ import { CurrencyService } from './currency.service';
 export class CronService {
   constructor(private readonly currencyService: CurrencyService) {}
 
-  @Cron(CronExpression.EVERY_MINUTE)
-  async handleCron() {
-    console.log('Add New Price');
-    const data = await this.currencyService.getPrice();
-    if (data) await this.currencyService.savePrice(data.price, data.datetime);
-  }
+  // @Cron(CronExpression.EVERY_MINUTE)
+  // async handleCron() {
+  //   console.log('Add New Price');
+  //   const data = await this.currencyService.getPrice();
+  //   if (data) await this.currencyService.savePrice(data.price, data.datetime);
+  // }
 }
